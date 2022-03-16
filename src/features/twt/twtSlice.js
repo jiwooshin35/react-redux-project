6d4b6fc5c9e26ad55446f7ab5ad7270bc9bb31f0
Jiwoo Shin <jiwooshin35@gmail.com>
2022-03-16 12:07:29 -0400

    commit

--- src/features/twt/twtSlice.js
+++ src/features/twt/twtSlice.js
@@ -1,11 +1,9 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { useSelector } from "react-redux";
-
-const token='AAAAAAAAAAAAAAAAAAAAAA4SaQEAAAAATir7fbZ2T3d6i9m8nrQSi%2FU1%2F3M%3DH8mhLJ50DiJ5UQ87RaM6sVco0ayZRqfGcLaO2WkYxJTDikZFcz'
+import { token } from "./token";
 export const loadTwts=createAsyncThunk('twt/loadTwts',
 async()=>{
-    try{
-        const response= await fetch(`https://cors-anywhere.herokuapp.com/https://api.twitter.com/2/users/44196397/tweets?&max_results=200`,
+    try{ //load 100 of elon musk's twts
+        const response= await fetch(`https://cors-anywhere.herokuapp.com/https://api.twitter.com/2/users/44196397/tweets?&max_results=100`,
         {
             headers:{
                 Authorization: `Bearer ${token}`}})
